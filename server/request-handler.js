3174b523db8d645e417ffd354820429e9eace245
khengschew <khengc@gmail.com>
2019-01-19 08:05:00 -0800

    Update request-handler to properly trigger request.end() with the right data

--- server/request-handler.js
+++ server/request-handler.js
@@ -67,32 +67,50 @@ exports.requestHandler = function(request, response) {
 
   var resData = {results:[]};
 
+  // Get data
+  // var body = '';
+  // request.on('data', (chunk) => {
+
+  //   body += chunk.toString('utf8');
+
+  // }).on('end', () => {
+
+  // });
+
   if (request.method === 'GET') {
 
     resData.results = storedData;
+
+    response.writeHead(statusCode, headers);
     response.end(JSON.stringify(resData));
 
   } else if (request.method === 'POST') {
 
     // Get data
-    let body = '';
+    var body = '';
     request.on('data', (chunk) => {
       body += chunk.toString('utf8');
     }).on('end', () => {
       // Perform operations on data
-      storedData.push(JSON.parse(body));
+      body = JSON.parse(body);
+      body.createdAt = Date.now();
+
+      storedData.push(body);
+
+      // change statusCode to 201
+      statusCode = 201;
+
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(resData));
     });
 
-    // change statusCode to 201
-    statusCode = 201;
-
   } else if (request.method === 'OPTIONS') {
-
+    response.end();
   }
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  // response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -101,7 +119,7 @@ exports.requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end(JSON.stringify(resData));
+  // response.end(JSON.stringify(resData));
 };
 
 
